3415f11d0b4cd3ac276f99afaa0329552397129c
Charlie Meyer <charlie.g.meyer@icloud.com>
2023-02-26 11:53:47 -0500

    cors sucks but i have a functioning xkcd api local

--- script.js
+++ script.js
@@ -32,54 +32,47 @@ function showBTCAddr() {
     copiedText.style.opacity=0;
     }, 2000);
 }
-/*
-window.onload = function() {
-    const xkcdUrl = 'https://xkcd.com/info.0.json';
-  
-    fetch(xkcdUrl)
-      .then(response => response.json())
-      .then(data => {
-        
-        const comicImg = document.createElement('img');
-        comicImg.style = ""
-        comicImg.src = data.img;
-        comicImg.alt = data.alt;
-        //document.body.appendChild(comicImg);
-        const titleAndDate = document.createElement('p');
-        titleAndDate.style = "text-align:center;"
-        titleAndDate.textContent = `Today's XKCD Comic: ${data.title} - ${new Date().toDateString()}`;
-        //document.body.appendChild(titleAndDate);
-        document.getElementById('photo-locator').appendChild(document.createElement('hr'))
-        document.getElementById('photo-locator').appendChild(titleAndDate)
-        document.getElementById('photo-locator').appendChild(comicImg)
-        
-      })
-      .catch(error => console.error(error));
-  };
-  */
 
-  window.onload = function() {
-    const xkcdUrl = 'https://xkcd.com/';
+  // window.onload = function() {
+  //   const xkcdUrl = 'https://xkcd.com/';
   
-    fetch(xkcdUrl)
-      .then(response => response.json())
-      .then(html => {
-        let parser = new DOMParser();
-        let doc = parser.parseFromString(html, "text/html");
-        let element = doc.getElementById("comic");
-        console.log(element.innerHTML());
+  //   fetch(xkcdUrl)
+  //     .then(response => response.json())
+  //     .then(html => {
+  //       let parser = new DOMParser();
+  //       let doc = parser.parseFromString(html, "text/html");
+  //       let element = doc.getElementById("comic");
+  //       console.log(element.innerHTML());
 
-        /*
-        //document.body.appendChild(comicImg);
-        const titleAndDate = document.createElement('p');
-        titleAndDate.style = "text-align:center;"
-        titleAndDate.textContent = `Today's XKCD Comic: ${data.title} - ${new Date().toDateString()}`;
-        //document.body.appendChild(titleAndDate);
-        document.getElementById('photo-locator').appendChild(document.createElement('hr'))
-        document.getElementById('photo-locator').appendChild(titleAndDate)
-        document.getElementById('photo-locator').appendChild(comicImg)
-        */
-      })
-      .catch(error => console.error(error));
-  };
+  //       /*
+  //       //document.body.appendChild(comicImg);
+  //       const titleAndDate = document.createElement('p');
+  //       titleAndDate.style = "text-align:center;"
+  //       titleAndDate.textContent = `Today's XKCD Comic: ${data.title} - ${new Date().toDateString()}`;
+  //       //document.body.appendChild(titleAndDate);
+  //       document.getElementById('photo-locator').appendChild(document.createElement('hr'))
+  //       document.getElementById('photo-locator').appendChild(titleAndDate)
+  //       document.getElementById('photo-locator').appendChild(comicImg)
+  //       */
+  //     })
+  //     .catch(error => console.error(error))
+  // };
+
+window.onload = function cringeaxios() {
+  const proxyURL = 'https://cors-anywhere.herokuapp.com/'
+  const xkcdURL = 'https://xkcd.com/info.0.json'
+  axios.get(proxyURL + xkcdURL).then((response) => {
+    console.log(JSON.stringify(response.data.img));
+
+    const title = document.getElementById('title');
+    const comic = document.getElementById('comic');
+    const caption = document.getElementById('caption')
+
+    title.style = "text-align:center";
+    title.textContent = `Today's XKCD Comic: ${response.data.title} - ${new Date().toDateString()}`
+    caption.textContent = response.data.alt
+    caption.style = "text-align:center; font-size:12px;"
+    comic.setAttribute('src', response.data.img)
+  })
+}
 
